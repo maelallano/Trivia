0777846946efb6bffd889f4056de90dd10312864
mallano <maelallano1@gmail.com>
2018-12-12 19:43:27 +0100

    first commit

---
+++ src/views/Category/CategoryContainer.js
@@ -0,0 +1,54 @@
+import React, { Component, createRef } from 'react';
+import api from '../../helpers/api';
+import Category from './Category';
+
+class CategoryContainer extends Component {
+  state = {
+    category: null,
+    currentQuestion: 0,
+  }
+
+  // createRef in order to bring back input value to its parent
+  answerInput = createRef();
+
+  // async needed when using promise
+  async componentDidMount() {
+    const data = await api.getCategoryById(this.props.match.params.id);
+    // stored response in the state;
+    this.setState({
+      category: data,
+    });
+  }
+
+  handleSubmit = (e) => {
+    // here I prevent the default bh of submitting form
+    e.preventDefault();
+    // write logic to handle good/bad answer
+    // increment currentQuestion
+    // save in the storage the id of the question
+    // if no more question, remove category from categories playable
+    // increment score somewhere and redirect to /
+
+    const answer = this.answerInput.current.value;
+    // check if answer is equal to the requested answer from the current question
+  }
+
+  render() {
+    const { category, currentQuestion } = this.state;
+    // at first render, category will be null so we need to wait
+    // before using data.
+    if (!category) return <div>is loading</div>
+
+    return (
+      <Category
+        category={category}
+        currentQuestionIndex={currentQuestion}
+        handleSubmit={this.handleSubmit}
+        // plug createRef to chidlren
+        answerInput={this.answerInput}
+      />
+    );
+  }
+}
+
+export default CategoryContainer;
